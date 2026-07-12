3bc6d4473577f317d8c5d4233aa022d76b91d632
preet-jain <preetjain2323@gmail.com>
2026-07-12 19:53:46 +0530

    Data Comparions
    Signed-off-by: preet-jain <preetjain2323@gmail.com>

---
+++ Basic/04_Comparison.js
@@ -0,0 +1,54 @@
+
+//console.log("zazzzzzzzzzzzzzzzz" >=  "zzzaaaaaaaaaaaaaaaaaaaaa1");
+
+/*
+in this comparsion happen character wise
+*/
+
+// console.log("2" > 1)
+// console.log("1102" > 11010)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null < 0)
+// console.log(null != 0)
+
+/*
+ so when we compare null with >,>=,<,<= the value of 
+ null is converted to 0 and then comparison happen
+
+ so when we compare null with ==,!= the value of null is not 
+ converted to 0 and then comparison happen
+
+*/
+
+//  console.log(undefined > 0)
+//  console.log(undefined == 0)
+//  console.log(undefined >= 0)
+//  console.log(undefined <= 0)
+//  console.log(undefined < 0)
+//  console.log(undefined != 0)
+
+
+ /*
+
+    so when we compare undefined with >,>=,<,<= the value of
+    undefined is converted to NaN and then comparison happen
+
+    so when we compare undefined with ==,!= the value of undefined is not
+    converted to NaN and then comparison happen
+
+ */
+
+
+    /* Clean Code is important for readability and maintainability of code we have to 
+    avoid that */
+    
+
+
+/*
+Never compare string with other datatype
+*/
