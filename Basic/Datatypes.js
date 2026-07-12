2b6974a43baa61afc5d0fb28e31c07d1cf36bc62
preet-jain <preetjain2323@gmail.com>
2026-07-12 17:37:07 +0530

    Explore the datatype
    Signed-off-by: preet-jain <preetjain2323@gmail.com>

---
+++ Basic/Datatypes.js
@@ -0,0 +1,30 @@
+"use strinct"  // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+let name = "preet";
+let age = 20
+let isAdult = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+
+console.log(name)
+console.log(age)
+console.log(isAdult)
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isAdult)
