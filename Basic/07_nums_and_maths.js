d2214fe78240db392882646a8e5eba20931c7898
preet-jain <preetjain2323@gmail.com>
2026-07-19 06:04:05 +0530

    Properties of the number and math library
    Signed-off-by: preet-jain <preetjain2323@gmail.com>

---
+++ Basic/07_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 100
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.89765
+// console.log(otherNumber.toPrecision(2))
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(6))
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//********************Maths *********************/
+
+// console.log(Math)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Math.max(10,8,7,5))
+// console.log(Math.min(1,2,-1,-2))
+// console.log((Math.random()*10)+1);
+let Min = 10
+let Max = 20
+console.log(Math.floor(Math.random()*(Max - Min + 1)) + Min)
+
+/*
+ Best logic for random  generation between number
+*/
+
+
+
+
+
